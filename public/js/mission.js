72d9f5d95f0bec74768da6bb17b08c6c94e304bb
David Toomey <d.toomey@temple.edu>
2015-06-26 17:37:24 -0400

    mission javascript file

---
+++ public/js/mission.js
@@ -0,0 +1,11 @@
+$(document).click(  
+            
+            function(){ $("body").fadeOut(2000);
+                        $("body").fadeIn(2000);
+                        $("h1").slideUp(2000);
+                        $("h1").slideDown(2000)
+                        $("p").slideDown(2000);
+                        
+                        
+
+                ('');   } );
